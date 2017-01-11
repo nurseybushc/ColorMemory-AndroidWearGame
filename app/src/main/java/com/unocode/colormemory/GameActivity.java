b50126f1ff65177fc464d40d6811cc3286e69f81
Chance Nursey-Bush <cnurseyb@asu.edu>
2017-01-11 00:45:03 -0700

    1/11/17
    *added score textview to game page that increase each time a color
    button is pressed
    *added icons for buttons on main page

--- app/src/main/java/com/unocode/colormemory/GameActivity.java
+++ app/src/main/java/com/unocode/colormemory/GameActivity.java
@@ -3,7 +3,10 @@ package com.unocode.colormemory;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * Created by Chance on 1/7/2017.
@@ -11,19 +14,28 @@ import android.widget.TextView;
 
 public class GameActivity extends Activity {
 
-    private TextView mTextView;
+    public TextView mTextView;
+    private Integer currentScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        currentScore = 0;
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView = (TextView) stub.findViewById(R.id.tvCurrentScore);
+                mTextView.setText(String.format(Locale.US,"%d",currentScore));
             }
         });
+    }
 
+    public void increaseScore(View v)
+    {
+        currentScore += 1;
+        mTextView.setText(String.format(Locale.US,"%d",currentScore));
     }
 }
